9584788acf05c52515603b8720b5581214f75315
Narendra Jethi <narendrajethi220@gmail.com>
2024-01-05 15:06:05 +0530

    Solution for maximum Wealth
    Space efficient code for the problem 1672. Richest Customer Wealth leetcode.

---
+++ Array/Easy/1672RichestCustomerWealth.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/richest-customer-wealth/
+
+//Space efficient Program 
+
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int max=INT_MIN;
+        for(int i=0;i<accounts.size();i++){
+        int sum=0;
+            for(int j=0;j<accounts[i].size();j++){
+              sum+=accounts[i][j];
+            }
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return max;
+        
+    }
+};
