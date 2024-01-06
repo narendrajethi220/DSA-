80db5955c512df4bcf9f7ee31a90a77a49640e2b
Narendra Jethi <narendrajethi220@gmail.com>
2024-01-06 11:17:51 +0530

    1431. Kids With the Greatest Number of Candies
    Solution for the problem 1431. Kids With the Greatest Number of Candies, Run time 0 ms Beats 100% of the user.

---
+++ Array/Easy/1431KidsWiththeGreatestNumberofCandies.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+
+class Solution {
+public:
+ 
+    bool isMax(int ele,int max){
+          if(ele>=max){
+              return true;
+          }
+          return false;
+  
+    }
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+       int max=INT_MIN;
+       vector<bool> ans(candies.size());
+       for(int i=0;i<candies.size();i++){
+           if(candies[i]>max){
+               max=candies[i];
+           }
+       }
+       for(int i=0;i<candies.size();i++){
+           if(isMax((candies[i]+extraCandies),max)){
+               ans[i]=true;
+           }
+           else{
+               ans[i]=false;
+       }
+     return ans;   
+    }
+};
