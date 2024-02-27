d9c609ddac1870ed0b549a868ec87d0a80a85c10
Narendra Jethi <narendrajethi220@gmail.com>
2024-02-27 16:31:26 +0530

    Reverse Linked List-II
    Optimize Approach

--- LinkedList/92ReverseLinkedListII.cpp
+++ LinkedList/92ReverseLinkedListII.cpp
@@ -40,3 +40,39 @@ public:
 return head;
     }
 };
+
+// Optimized Approach
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        ListNode* dummy=new ListNode(0,head);
+        ListNode* temp=head;
+        ListNode* leftPrev=dummy;
+        for(int i=0;i<left-1;i++){
+            leftPrev=leftPrev->next;
+            temp=temp->next;
+        }
+        ListNode* saveLNode=temp;
+        ListNode* prev=NULL;
+        for(int i=0;i<=(right-left);i++){
+            ListNode* nextNode=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=nextNode;
+        }
+        leftPrev->next=prev;
+        saveLNode->next=temp;
+        return dummy->next;
+
+    }
+};
